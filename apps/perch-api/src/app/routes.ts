b446f77a21dc8766ec5b8c60ffae87c01c946632
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2025-06-12 14:46:07 -0500

    Add middlewares and Error response interface

--- apps/perch-api/src/app/routes.ts
+++ apps/perch-api/src/app/routes.ts
@@ -1,10 +1,13 @@
 // apps/perch-api/src/app/routes.ts
 import { Router } from 'express';
+import { taskRoutes } from './modules/task/task.routes';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hello from Perch API!' });
 });
+
+router.use('/tasks', taskRoutes);
 
 export default router;
